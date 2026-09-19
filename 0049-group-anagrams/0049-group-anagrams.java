b701adfe86a0e51fbadf16719f8d721ed5b79deb
shashank gadhvi <shashank.gadhvi2006@gmail.com>
2026-09-19 11:49:33 +0530

    Time: 7 ms (71.87%), Space: 49.5 MB (72.8%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,16 +1,15 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-
-       HashMap<String,List<String>> map = new HashMap<>();
+        HashMap<String,List<String>> map = new HashMap<>();
 
         for(int i=0;i<strs.length;i++){
-           char[] chars = strs[i].toCharArray();
-           Arrays.sort(chars); // we sort the given iterating element of array
-           String sorted= new String(chars);
-           List<String> list = map.getOrDefault(sorted,new ArrayList<>());
-           list.add(strs[i]);
-           map.put(sorted,list);
+            char[] char_array = strs[i].toCharArray();
+            Arrays.sort(char_array);
+            String sorted_form_string = new String(char_array);
+            List<String> list = map.getOrDefault(sorted_form_string,new ArrayList<>());
+            list.add(strs[i]);
+            map.put(sorted_form_string,list);
         }
-        return new ArrayList(map.values());
+    return new ArrayList(map.values());
     }
 }
